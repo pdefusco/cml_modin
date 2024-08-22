acc3b985c4efb150639f1ad5ef4f13d61efb6efa
pdefusco <pauldefusco@gmail.com>
2024-08-21 17:47:43 -0700

    datagen

--- 00_datagen.py
+++ 00_datagen.py
@@ -39,65 +39,146 @@
 
 import os
 import numpy as np
-import pandas as pd
 from datetime import datetime
 import dbldatagen as dg
-import dbldatagen.distributions as dist
-from dbldatagen import FakerTextFactory, DataGenerator, fakerText
-from faker.providers import bank, credit_card, currency
+from pyspark.sql import SparkSession
+from dbldatagen import DataGenerator
 from pyspark.sql.types import LongType, FloatType, IntegerType, StringType, \
                               DoubleType, BooleanType, ShortType, \
                               TimestampType, DateType, DecimalType, \
                               ByteType, BinaryType, ArrayType, MapType, \
                               StructType, StructField
 
-class BankDataGen:
+class DataGen:
 
     '''Class to Generate Banking Data'''
 
     def __init__(self, spark):
         self.spark = spark
 
-    def transactionsDataGen(self, shuffle_partitions_requested = 10, partitions_requested = 10, data_rows = 10000):
-
-        # setup use of Faker
-        FakerTextUS = FakerTextFactory(locale=['en_US'], providers=[bank])
+    def dataGen(self, shuffle_partitions_requested = 10, partitions_requested = 10, data_rows = 10000):
 
         # partition parameters etc.
         self.spark.conf.set("spark.sql.shuffle.partitions", shuffle_partitions_requested)
 
         fakerDataspec = (DataGenerator(self.spark, rows=data_rows, partitions=partitions_requested)
-                    .withColumn("credit_card_number", text=FakerTextUS("credit_card_number") )
-                    .withColumn("credit_card_provider", text=FakerTextUS("credit_card_provider") )
-                    .withColumn("transaction_type", "string", values=["purchase", "cash_advance"], random=True, weights=[9, 1])
-                    .withColumn("event_ts", "timestamp", begin="2023-01-01 01:00:00",end="2023-12-31 23:59:00",interval="1 minute", random=True)
-                    .withColumn("longitude", "float", minValue=-125, maxValue=-66.9345, random=True)
-                    .withColumn("latitude", "float", minValue=24.3963, maxValue=49.3843, random=True)
-                    .withColumn("transaction_currency", values=["USD", "EUR", "KWD", "BHD", "GBP", "CHF", "MEX"])
-                    .withColumn("transaction_amount", "decimal", minValue=0.01, maxValue=30000, random=True)
+                    .withColumn("col1", values=["USD", "EUR", "KWD", "BHD", "GBP", "CHF", "MEX"])
+                    .withColumn("col2", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col3", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col4", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col5", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col6", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col7", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col8", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col9", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col10", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col11", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col12", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col13", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col14", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col15", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col16", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col17", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col18", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col19", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col20", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col21", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col22", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col23", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col24", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col25", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col26", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col27", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col28", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col29", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col30", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col31", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col32", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col33", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col34", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col35", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col36", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col37", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col38", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col39", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col40", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col41", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col42", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col43", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col44", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col45", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col46", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col47", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col48", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col49", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col50", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col51", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col52", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col53", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col54", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col55", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col56", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col57", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col58", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col59", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col60", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col61", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col62", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col63", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col64", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col65", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col66", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col67", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col68", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col69", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col70", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col71", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col72", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col73", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col74", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col75", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col76", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col77", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col78", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col79", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col80", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col81", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col82", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col83", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col84", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col85", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col86", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col87", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col88", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col89", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col90", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col91", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col92", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col93", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col94", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col95", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col96", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col97", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col98", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col99", "float", minValue=1, maxValue=10000000, random=True)
+                    .withColumn("col100", "float", minValue=1, maxValue=10000000, random=True)
                     )
 
         df = fakerDataspec.build()
 
         return df
 
+spark = SparkSession \
+    .builder \
+    .appName("DATA GENERATION") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")\
+    .config("spark.sql.catalog.spark_catalog.type", "hive")\
+    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
+    .getOrCreate()
 
+modinDG = DataGen(spark)
 
-import cml.data_v1 as cmldata
-
-# Sample in-code customization of spark configurations
-from pyspark import SparkContext
-SparkContext.setSystemProperty('spark.executor.cores', '1')
-SparkContext.setSystemProperty('spark.executor.memory', '2g')
-
-CONNECTION_NAME = "rags-italia-aw-dl"
-conn = cmldata.get_connection(CONNECTION_NAME)
-spark = conn.get_spark_session()
-
-myDG = BankDataGen(spark)
-
-STORAGE="s3a://rags-italia-buk-c2b5a457/data/"
-transactionsDf = myDG.transactionsDataGen()
-transactionsDf = transactionsDf.repartition(1)
-transactionsDf.write.format("csv").mode("overwrite").save(STORAGE+"pdefusco/transactions/trans")
+STORAGE="s3a://paul-tests-buk-8cfc06be/data/"
+sparkDf = modinDG.dataGen()
+sparkDf.write.format("parquet").mode("overwrite").save(STORAGE+"pdefusco/modin/data")
 #transactionsDf.write.format("json").mode("overwrite").save("/home/cdsw/jsonData2.json")
--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Modin uses Ray, Dask or Unidist to provide an effortless way to speed up your pa
 [Modin Docs](https://modin.readthedocs.io/en/latest/index.html)
 [Modin Paper](https://arxiv.org/pdf/2001.00888)
 [Starting Up a Modin Cluster](https://modin.readthedocs.io/en/latest/getting_started/using_modin/using_modin_cluster.html)
+
+### CDE Data Generation Steps
+
+```
+cde credential create --name dckr-crds-modin --type docker-basic --docker-server hub.docker.com --docker-username $docker_user -v
+cde resource create --name ge-runtime-modin --image pauldefusco/dex-spark-runtime-3.2.3-7.2.15.8:1.20.0-b15-great-expectations-data-quality --image-engine spark3 --type custom-runtime-image -v
+
+cde resource create --name modin-files -v
+cde resource upload --name modin-files --local-path 00_datagen.py
+
+cde job create --name modin-datagen --type spark --application-file 00_datagen.py --mount-1-resource modin-files
+cde job run --name modin-datagen --driver-cores 5 --driver-memory "10g" --executor-cores 5 --executor-memory "20g"
+```
